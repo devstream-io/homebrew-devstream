59c00301b5fba9b227ae803348a47dda5178a7fd
Kaitian Xie <kaitian96@outlook.com>
2022-03-28 22:42:38 +0800

    fix dev-stream.rb

--- Formula/dev-stream.rb
+++ Formula/dev-stream.rb
@@ -1,6 +1,6 @@
 class DevStream < Formula
-  desc "DevStream: the open-source DevOps toolchain manager (DTM)."
-  homepage ""
+  desc "Open-source DevOps toolchain manager (DTM)"
+  homepage "https://github.com/merico-dev/stream"
   url "https://github.com/merico-dev/stream/archive/refs/tags/v0.3.0.tar.gz"
   sha256 "3223ff4ea46626d6849831ad9abfa6f67699731fd927fe477ab945b3ab9424eb"
   license "Apache-2.0"
@@ -12,12 +12,12 @@ class DevStream < Formula
     goarch = Utils.safe_popen_read("#{Formula["go"].bin}/go", "env", "GOARCH").chomp
     ENV["GOPATH"] = buildpath
     ENV["GO111MODULE"] = "auto"
-    system "mkdir", "-p", buildpath/"bin"
+    mkdir_p buildpath/"bin"
     ENV.prepend_path "PATH", buildpath/"bin"
     (buildpath/"src/github.com/merico-dev/stream").install buildpath.children
     cd "src/github.com/merico-dev/stream" do
       system "make", "build"
-      system "mv", "dtm-#{goos}-#{goarch}", "dtm"
+      mv "dtm-#{goos}-#{goarch}", "dtm"
       bin.install "dtm"
     end
   end
